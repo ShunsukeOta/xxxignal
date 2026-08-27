0f11b20ee54efdf0dc6b1a6c07a8d88ceb741268
Shunsuke Ota <81334237+ShunsukeOta@users.noreply.github.com>
2026-08-27 15:08:03 +0900

    fix: restore base64url regex escaping

--- src/worker/lib/crypto.ts
+++ src/worker/lib/crypto.ts
@@ -4,7 +4,7 @@ import { AppError } from './http'
 function base64Url(bytes: Uint8Array) {
   let binary = ''
   for (const byte of bytes) binary += String.fromCharCode(byte)
-  return btoa(binary).replace(/+/g, '-').replace(///g, '_').replace(/=+$/g, '')
+  return btoa(binary).replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/g, '')
 }
 
 function fromBase64(value: string) {
